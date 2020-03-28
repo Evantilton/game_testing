18865e50ec34ce57edf87fae3ee1aeb97237b885
evantilton <evantilton@gmail.com>
2020-03-28 17:30:11 -0500

    got an enemy piece to move down every second

--- game.js
+++ game.js
@@ -9,11 +9,31 @@ const matrix = [
     [1, 1, 1,],
 ];
 
+const enemyMatrix = [
+    [1, 0, 1,],
+    [0, 1, 0,],
+    [1, 0, 1,],
+]
+function drawEnemyMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = '#0DC2FF';
+                context.fillRect(x + offset.x,
+                    y + offset.y,
+                    1, 1);
+            }
+        })
+    })
+};
+
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     drawMatrix(player.matrix, player.pos);
+    drawEnemyMatrix(enemy.matrix, enemy.pos);
+
 }
 
 function drawMatrix(matrix, offset) {
@@ -39,14 +59,17 @@ function update(time = 0) {
 
     dropCounter += deltaTime;
     if (dropCounter > dropInterval) {
-        player.pos.y++;
+        enemy.pos.y++;
         dropCounter = 0;
     }
     draw();
     requestAnimationFrame(update);
 }
 
-
+function enemyDrop() {
+    enemy.pos.y++;
+    dropCounter = 0;
+}
 document.addEventListener('keydown', event => {
     console.log(event);
     if (event.keyCode === 37) {
@@ -59,6 +82,11 @@ document.addEventListener('keydown', event => {
         player.pos.y--;
     } 
 });
+
+const enemy = {
+    pos: { x: 28, y: 1},
+    matrix: enemyMatrix,
+}
 
 const player = {
     pos: { x: 28, y: 50 },
