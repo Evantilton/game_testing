cdb77195eed938c689b45c479bb5c7553e81cd28
evantilton <evantilton@gmail.com>
2020-03-28 17:02:19 -0500

    got it to draw and update the board properly when I redraw the player character

--- game.js
+++ game.js
@@ -1,21 +1,42 @@
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
 
-context.scale(50,50);
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+context.scale(10, 10);
 
 const matrix = [
     [0, 1, 0,],
     [0, 1, 0,],
-    [1, 1, 1,], 
+    [1, 1, 1,],
 ];
 
-matrix.forEach((row, y) => {
-    row.forEach((value, x) => {
-        if (value !== 0) {
-            context.fillStyle = 'red';
-            context.fillRect(x, y, 1, 1)
-        }     
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = '#0DC2FF';
+                context.fillRect(x + offset.x,
+                    y + offset.y,
+                    1, 1);
+            }
+        })
     })
-});
+};
+
+function update() {
+    draw();
+    requestAnimationFrame(update);
+}
+
+const player = {
+    pos: { x: 28, y: 50 },
+    matrix: matrix,
+}
+
+update();
